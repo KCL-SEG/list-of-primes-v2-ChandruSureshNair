ffc8f7b5e9149cb6531794c39543788f5d0475cd
Chandru Suresh <k21012031@kcl.ac.uk>
2022-10-02 21:14:15 +0100

    Implement primes method

--- primes.py
+++ primes.py
@@ -3,4 +3,16 @@
 
 def primes(number_of_primes):
     list = []
+
+    n = 2
+    while len(list) < number_of_primes:
+        factors = False
+        i = 2
+        while not factors and i < n:
+            factors = n % i == 0
+            i += 1
+        if not factors:
+            list.append(n)
+        n += 1
+
     return list
